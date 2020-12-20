585aab4055e4fc7b9575027ca04c1dcd380b212e
秋水 <1123993881@qq.com>
2020-12-20 20:30:59 +0800

    Add a sortable item

---
+++ RadiWind/Sort/SortableItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RadiWind.Sort
+{
+    public struct SortableItem<T>
+    {
+        public int Index { get; }
+        public T Value { get; set; }
+        public SortableItem(int index, T value )
+        {
+            this.Index = index;
+            this.Value = value;
+        }
+    }
+}
