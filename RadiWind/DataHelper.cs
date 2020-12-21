9ebb664b9e015de5a579108e0bcd717f4bcb7b3c
秋水 <1123993881@qq.com>
2020-12-21 16:10:04 +0800

    Add Head LIcense.

--- RadiWind/DataHelper.cs
+++ RadiWind/DataHelper.cs
@@ -1,4 +1,11 @@
-﻿using Grasshopper;
+﻿/*  Copyright 2020 RadiRhino-秋水, 笑里追风. All Rights Reserved.
+
+    Distributed under MIT license.
+
+    See file LICENSE for detail or copy at http://opensource.org/licenses/MIT
+*/
+
+using Grasshopper;
 using System;
 using System.Collections.Generic;
 using System.Linq;
--- RadiWind/Sort/SortPointInAxisWithToleranceComponent.cs
+++ RadiWind/Sort/SortPointInAxisWithToleranceComponent.cs
@@ -1,4 +1,11 @@
-﻿using Grasshopper.Kernel;
+﻿/*  Copyright 2020 RadiRhino-秋水, 笑里追风. All Rights Reserved.
+
+    Distributed under MIT license.
+
+    See file LICENSE for detail or copy at http://opensource.org/licenses/MIT
+*/
+
+using Grasshopper.Kernel;
 using Grasshopper.Kernel.Parameters;
 using Rhino.Geometry;
 using System;
