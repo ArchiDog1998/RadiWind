94c05b2c0cb3edb2d9100c92b08912a97256adcc
秋水 <1123993881@qq.com>
2020-12-21 16:46:21 +0800

    Add a Plane Coordinate.

---
+++ RadiWind.Tests/PlaneServerTests.cs
@@ -0,0 +1,34 @@
+﻿/*  Copyright 2020 RadiRhino-秋水, 笑里追风. All Rights Reserved.
+
+    Distributed under MIT license.
+
+    See file LICENSE for detail or copy at http://opensource.org/licenses/MIT
+*/
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Rhino.Geometry;
+using RadiWindAlgorithm;
+
+namespace RadiWind.Tests
+{
+    /// <summary>
+    /// Summary description for UnitTest1
+    /// </summary>
+    [TestClass]
+    public class PlaneServerTests
+    {
+
+        [TestMethod]
+        public void PlaneCoordinateTest()
+        {
+            Point3d testPt = new Point3d(10, 20, 30);
+
+            Point3d resultPt = PlaneServer.PlaneCoordinate(Plane.WorldXY, testPt);
+
+            Assert.AreEqual(testPt, resultPt);
+        }
+    }
+}
---
+++ RadiWindAlgorithm/PlaneServer.cs
@@ -0,0 +1,32 @@
+﻿/*  Copyright 2020 RadiRhino-秋水, 笑里追风. All Rights Reserved.
+
+    Distributed under MIT license.
+
+    See file LICENSE for detail or copy at http://opensource.org/licenses/MIT
+*/
+
+using Rhino.Geometry;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RadiWindAlgorithm
+{
+    public static class PlaneServer
+    {
+        public static Point3d PlaneCoordinate(Plane basePlane, Point3d ptInWorld)
+        {
+            double x = 0;
+            double y = 0;
+            basePlane.ClosestParameter(ptInWorld, out x, out y);
+
+            Point3d closestPt = basePlane.ClosestPoint(ptInWorld);
+            double z = closestPt.DistanceTo(ptInWorld);
+            //double z = basePlane.DistanceTo(ptInWorld);
+
+            return new Point3d(x, y, z);
+        }
+    }
+}
